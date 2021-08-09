dd8cb68f917de91f6cf3283f666a4dabbafee3a1
Sam <samb-t@hotmail.co.uk>
2021-08-09 12:22:12 +0100

    Changed up/downsampling weight init

--- models/vqgan_v2.py
+++ models/vqgan_v2.py
@@ -156,6 +156,44 @@ class GumbelQuantizer(nn.Module):
             'min_encoding_indices': min_encoding_indices
             }
 
+def variance_scaling(scale, mode, distribution,
+                     in_axis=1, out_axis=0,
+                     dtype=torch.float32,
+                     device='cpu'):
+  """Ported from JAX. """
+
+  def _compute_fans(shape, in_axis=1, out_axis=0):
+    receptive_field_size = np.prod(shape) / shape[in_axis] / shape[out_axis]
+    fan_in = shape[in_axis] * receptive_field_size
+    fan_out = shape[out_axis] * receptive_field_size
+    return fan_in, fan_out
+
+  def init(shape, dtype=dtype, device=device):
+    fan_in, fan_out = _compute_fans(shape, in_axis, out_axis)
+    if mode == "fan_in":
+      denominator = fan_in
+    elif mode == "fan_out":
+      denominator = fan_out
+    elif mode == "fan_avg":
+      denominator = (fan_in + fan_out) / 2
+    else:
+      raise ValueError(
+        "invalid mode for variance scaling initializer: {}".format(mode))
+    variance = scale / denominator
+    if distribution == "normal":
+      return torch.randn(*shape, dtype=dtype, device=device) * np.sqrt(variance)
+    elif distribution == "uniform":
+      return (torch.rand(*shape, dtype=dtype, device=device) * 2. - 1.) * np.sqrt(3 * variance)
+    else:
+      raise ValueError("invalid distribution for variance scaling initializer")
+
+  return init
+
+def default_init(scale=1.):
+  """The same initialization used in DDPM."""
+  scale = 1e-10 if scale == 0 else scale
+  return variance_scaling(scale, 'fan_avg', 'uniform')
+
 def _setup_kernel(k):
     k = np.asarray(k, dtype=np.float32)
     if k.ndim == 1:
@@ -265,7 +303,7 @@ class Downsample(nn.Module):
     def __init__(self, in_channels,fir_kernel=(1, 3, 3, 1)):
         super().__init__()
         self.Conv2d = Conv2dFIR(in_channels, in_channels, kernel=3, down=True, 
-                                resample_kernel=fir_kernel, use_bias=True)
+                                resample_kernel=fir_kernel, use_bias=True, kernel_init=default_init())
 
     def forward(self, x):
         return self.Conv2d(x)
@@ -275,7 +313,7 @@ class Upsample(nn.Module):
     def __init__(self, in_channels, fir_kernel=(1, 3, 3, 1)):
         super().__init__()
         self.Conv2d = Conv2dFIR(in_channels, in_channels, kernel=3, up=True,
-                                resample_kernel=fir_kernel, use_bias=True)
+                                resample_kernel=fir_kernel, use_bias=True, kernel_init=default_init())
 
     def forward(self, x):
         return self.Conv2d(x)
